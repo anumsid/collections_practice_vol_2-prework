4bfd14c48148fe30153581a917f1c9dda62d547b
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-16 22:56:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
-# your code goes here
+require "pry"
+def begins_with_r(array)
+   array.all? do |word|
+    word.downcase.start_with?("r")
+  end
+end
+
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+    end
+end
+
+def first_wa(array)
+  array.find do |word|
+    word.downcase.to_s.start_with?("wa")
+  end
+end
+
+def remove_non_strings(array)
+  array.reject do |word|
+     word.class != String
+  end
+end
+
+def count_elements(array)
+hash = {}
+ array.each do |ele|
+    name = ele[:name]
+    if !hash[name]
+      hash[name] = 1
+    else
+      hash[name] += 1
+    end
+   end
+  hash.map do |k, v|
+   {name:k, count:v}
+    #binding.pry
+  end
+ end
+
+ # def count_elements(array)
+ # hash = {}
+ #  array.each {|ele| !hash[ele[:name]] ? hash[ele[:name]] = 1 : hash[ele[:name]] += 1}
+ #   hash.map {|k, v| {name:k, count:v}}
+ # end
+
+ def merge_data(keys, data)
+    i = 0
+   while i < keys.length
+     binding.pry
+     keys[i].merge(data[i][keys[:first_name]])
+     i += 1
+   end
